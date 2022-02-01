e84c8460e7152bc2e0758231b1c3081c8eaeb38c
Risda Tamam Aljava <risda123tamam@student.esaunggul.ac.id>
2022-02-02 01:04:59 +0700

    Update up to WTP

---
+++ warehouse/staff/hapus.php
@@ -0,0 +1,6 @@
+<?php
+include "koneksi.php";
+$id    = mysqli_real_escape_string($koneksi,$_GET['id']);
+$query = mysqli_query($koneksi,"DELETE FROM data_file WHERE id='$id' ");
+header('location:index.php?pesan=hapus-berhasil');
+?>
